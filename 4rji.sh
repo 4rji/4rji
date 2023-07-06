4e4bfe9880b307e14616982f166f10f4c9a1be2a
4rji <118249932+4rji@users.noreply.github.com>
2023-07-06 18:44:59 -0500

    Primera parte

---
+++ 4rji.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+# Descargar el repositorio
+git clone https://github.com/xJackSx/BSPWMkali.git
+
+# Navegar al directorio clonado
+cd BSPWMkali
+
+# Dar permisos de ejecución al archivo install.sh
+chmod +x install.sh
+
+# Ejecutar el script de instalación
+./install.sh
