2f4946aa54dc3f53927f597e8210370270eebdb0
4rji <118249932+4rji@users.noreply.github.com>
2023-07-13 17:50:52 -0500

    Update fix-4rji.sh

--- fix-4rji.sh
+++ fix-4rji.sh
@@ -11,10 +11,10 @@ sudo apt-get autoclean
 sudo apt-get autoremove
 
 # Forzar la instalación de la versión requerida de gnustep-base-common
-sudo apt-get install gnustep-base-common=1.29.0-4
+sudo apt-get install gnustep-base-common=1.29.0-4 -y
 
 # Intentar instalar el paquete gnustep-base-runtime
-sudo apt install gnustep-base-runtime
+sudo apt install gnustep-base-runtime -y
 
 # Opcional: Intentar instalar el paquete libgnustep-base1.29
 # sudo apt install libgnustep-base1.29
