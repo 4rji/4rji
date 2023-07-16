0daf3968a4952606094e6d2fd4071a42ee8035f0
4rji <118249932+4rji@users.noreply.github.com>
2023-07-15 23:02:50 -0500

    alias

--- 4rji-2.sh
+++ 4rji-2.sh
@@ -76,6 +76,9 @@ echo "pgg" | cat - ~/.zshrc > temp && mv temp ~/.zshrc
 echo "alias apagar='sudo shutdown -h now'"  >> ~/.zshrc
 echo "alias 4rji='git clone https://github.com/4rji/4rji.git'"  >> ~/.zshrc
 echo "alias fixsc='cd $HOME/4rji/fixed'" >> ~/.zshrc
+echo "alias bateria='acpi -b'" >> ~/.zshrc
+
+
 
 chmod +x ~/.config/bin/ip.sh
 cd
--- fixed/alias.sh
+++ fixed/alias.sh
@@ -7,3 +7,4 @@ echo "pgg" | cat - ~/.zshrc > temp && mv temp ~/.zshrc
 echo "alias apagar='sudo shutdown -h now'"  >> ~/.zshrc
 echo "alias 4rji='git clone https://github.com/4rji/4rji.git'"  >> ~/.zshrc
 echo "alias fixsc='cd $HOME/4rji/fixed'" >> ~/.zshrc
+echo "alias bateria='acpi -b'" >> ~/.zshrc
