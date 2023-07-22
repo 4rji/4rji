e6f5a6067d5a5a3be5e66eabffb1ad7bbc49164f
4rji <118249932+4rji@users.noreply.github.com>
2023-07-22 01:25:13 -0500

    updas

--- 4rji-2.sh
+++ 4rji-2.sh
@@ -77,6 +77,9 @@ echo "alias fixsc='cd $HOME/4rji/fixed'" >> ~/.zshrc
 echo "alias bateria='acpi -b'" >> ~/.zshrc
 echo "alias mkdirr='function _mkdirr(){ mkdir "$1" && cd "$1" };_mkdirr'"  >> ~/.zshrc
 echo "alias 4rji='f(){ git clone https://github.com/4rji/4rji.git && cd 4rji; unset -f f; }; f'"  >> ~/.zshrc
+echo "alias redr='sudo systemctl restart NetworkManager'"  >> ~/.zshrc
+echo "alias dormir='sudo systemctl suspend'"  >> ~/.zshrc
+
 
 
 chmod +x ~/.config/bin/ip.sh
