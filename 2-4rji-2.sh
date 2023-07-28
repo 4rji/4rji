7eb3dc927ae4916931fd0bc38f3ee6d2012dfc43
4rji <118249932+4rji@users.noreply.github.com>
2023-07-28 15:37:41 -0500

    Update 2-4rji-2.sh

--- 2-4rji-2.sh
+++ 2-4rji-2.sh
@@ -2,27 +2,22 @@
 
 
 # adding wallpaper for me
-mv Wallpaper ~/.config
+
+#login picture #Directorio de las imagenes del login in
+#cd /usr/share/desktop-base/kali-theme/login/
+
+sudo cp ~/Wallpaper/background.png /usr/share/desktop-base/kali-theme/login/background
+
 
 #directorio de inicio de sesion de del grub
 #sudo cp -r background.png /boot/grub/themes/kali/grub-4x3.png
 #sudo cp -r background.png /boot/grub/themes/kali/grub-16x9.png
-sudo cp -r ~/.config/Wallpaper/jack-dragon2_2048x2048.png /boot/grub/themes/kali/grub-16x9.png
-sudo cp -r ~/.config/Wallpaper/background4x3.png /boot/grub/themes/kali/grub-4x3.png
-sudo cp -r ~/.config/Wallpaper/jack-dragon2_2048x2048.png /boot/grub/background.png
+sudo cp -r ~/Wallpaper/jack-dragon2_2048x2048.png /boot/grub/themes/kali/grub-16x9.png
+sudo cp -r ~/Wallpaper/background4x3.png /boot/grub/themes/kali/grub-4x3.png
+sudo cp -r ~/Wallpaper/jack-dragon2_2048x2048.png /boot/grub/background.png
 sudo update-grub
 
-
-
-#Directorio de las imagenes del login in
-#cd /usr/share/desktop-base/kali-theme/login/
-
-
-sudo cp ~/.config/Wallpaper/background.png /usr/share/desktop-base/kali-theme/login/background
-
-
-
-
+mv ~/Wallpaper ~/.config
 
 
 
