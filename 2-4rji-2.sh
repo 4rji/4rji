02625f28cae3d00973b2113adc0dc270d004c89b
4rji <118249932+4rji@users.noreply.github.com>
2023-09-13 23:44:05 -0500

    Update 2-4rji-2.sh

--- 2-4rji-2.sh
+++ 2-4rji-2.sh
@@ -98,14 +98,13 @@ echo "alias notas='ranger $HOME/notas'"  >> ~/.zshrc
 
 echo "pgg" | cat - ~/.zshrc > temp && mv temp ~/.zshrc
 echo "alias apagar='sudo shutdown -h now'"  >> ~/.zshrc
-echo "alias 4rji='git clone https://github.com/4rji/4rji.git'"  >> ~/.zshrc
 echo "alias fixsc='cd $HOME/4rji/fixed'" >> ~/.zshrc
 echo "alias bateria='acpi -b'" >> ~/.zshrc
 echo "alias bateria-f='upower -i /org/freedesktop/UPower/devices/battery_BAT0'" >> ~/.zshrc
 
 
 echo "alias mkdirr='function _mkdirr(){ mkdir "$1" && cd "$1" };_mkdirr'"  >> ~/.zshrc
-echo "alias 4rji='f(){ git clone https://github.com/4rji/4rji.git && cd 4rji/fixed/; unset -f f; }; f'"  >> ~/.zshrc
+echo "alias 4rji='f(){ git clone https://github.com/4rji/4rji.git && cd 4rji/; unset -f f; }; f'"  >> ~/.zshrc
 echo "alias redr='sudo systemctl restart NetworkManager'"  >> ~/.zshrc
 echo "alias dormir='sudo systemctl suspend'"  >> ~/.zshrc
 echo "alias redr='sudo systemctl restart NetworkManager'"  >> ~/.zshrc
