802e1ce4187e4acdf385f43ab66d5a80793b7836
Havi Arji <haviarji@minito.local>
2023-10-12 00:36:59 -0500

    Create arch_4rji.sh

---
+++ arch_4rji.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+sudo apt install lsd -y
+
+# Descargar el repositorio
+git clone https://github.com/4rji/polybar.git
+
+# Navegar al directorio clonado
+cd polybar
+
+# Dar permisos de ejecución al archivo install.sh
+chmod +x install.sh
+
+# Ejecutar el script de instalación
+./install.sh
+
+
+
+sleep 4
+
+kill -9 -1
+
+
+#sudo rm -f 4rji.sh
+
+#kill -9 -1
