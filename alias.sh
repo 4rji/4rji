820d29d769d3b36b0531ada09a88178b901112ee
4rji <118249932+4rji@users.noreply.github.com>
2023-11-12 15:41:54 -0800

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -131,7 +131,7 @@ echo "alias dockercp='echo "ejecutar lo siguiente: docker cp ruta/del/archivo.tx
 echo "alias osr='cat /etc/os-release'" >> ~/.zshrc
 echo "alias chator='onionshare-cli --chat --public -v'" >> ~/.zshrc
 echo "alias catr='batcat -l rb '" >> ~/.zshrc
-echo "alias ddtest='dd if=/dev/zero of=testfile bs=1M count=1000 conv=fdatasync'" >> ~/.zshrc
+echo "alias ddtest='dd if=/dev/zero of=testfile bs=10M count=1000 conv=fdatasync'" >> ~/.zshrc
 
 
 
