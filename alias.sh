ad84b140cd28937eb0149a00e596939085f32989
4rji <118249932+4rji@users.noreply.github.com>
2023-11-13 23:50:16 -0800

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -130,7 +130,6 @@ echo "alias osr='cat /etc/os-release'" >> ~/.zshrc
 echo "alias chator='onionshare-cli --chat --public -v'" >> ~/.zshrc
 echo "alias catr='batcat -l rb '" >> ~/.zshrc
 echo "alias ddtest='dd if=/dev/zero of=testfile bs=10M count=1000 conv=fdatasync'" >> ~/.zshrc
-
 echo "alias rm='trash'"  >> ~/.zshrc
 echo "alias rml='trash-list'" >> ~/.zshrc
 echo "alias rme='trash-empty'" >> ~/.zshrc
