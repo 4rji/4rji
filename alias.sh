0e88a8c1480ac71aef2bf3711c61fb1f27e97efe
4rji <118249932+4rji@users.noreply.github.com>
2023-11-21 21:51:10 -0800

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -134,6 +134,7 @@ echo "alias velocidad='curl -s https://raw.githubusercontent.com/sivel/speedtest
 
 echo "alias bye='kill -9 -1 '" >> ~/.zshrc
 
+echo "alias conexiones='nmcli connection show --active'" >> ~/.zshrc
 
 
 
